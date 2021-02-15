523598ae0637ed363d0f74833fbc9e55addb1270
Karim Makhloufi <maclouf@gmail.com>
2021-02-15 16:30:17 +0100

    Permissions

--- Components/CameraTab.js
+++ Components/CameraTab.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Camera } from "expo-camera";
 
 import AppContainer from "./AppContainer";
 
 const CameraTabComponent = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    console.log("test");
+    return <Text>No access to camera</Text>;
+  }
   return (
     <AppContainer>
       <Text>Camera component</Text>
